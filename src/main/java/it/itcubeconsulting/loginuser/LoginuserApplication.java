bad550586911112e04a5f5b3e86f85014284a5bd
Ardit Shalla <ardit.shalla@itcubeconsulting.com>
2022-04-11 16:53:47 +0200

    Initial commit

---
+++ src/main/java/it/itcubeconsulting/loginuser/LoginuserApplication.java
@@ -0,0 +1,13 @@
+package it.itcubeconsulting.loginuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/itcubeconsulting/loginuser/LoginuserApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itcubeconsulting.loginuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
